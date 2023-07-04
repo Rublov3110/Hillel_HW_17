224728da3d012d9dfa735545518b2408f3fede2b
unknown <timmy31101994@gmail.com>
2023-07-04 19:36:41 +0300

    HW_18_minimal API

--- Hillel_HW_12/Program.cs
+++ Hillel_HW_12/Program.cs
@@ -1,4 +1,9 @@
 using Hillel_HW_12;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,5 +30,17 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapGet("/v2/myFamiliar/{name}/{surname}",
+    (HttpContext requestDelegate) =>
+    {
+        var name = requestDelegate.GetRouteValue("name")!.ToString()!;
+        var surname = requestDelegate.GetRouteValue("surname")!.ToString()!;
+        var service = requestDelegate.RequestServices.GetService<IMyFamiliarRegister>()!;
+        var myFamiliar = service.GetMyFamiliarName(name, surname);
+        if (myFamiliar == null) return Results.NoContent();
+        return Results.Ok(myFamiliar);
+    })
+    .WithName("Test")
+    .WithOpenApi();
 
 app.Run();
